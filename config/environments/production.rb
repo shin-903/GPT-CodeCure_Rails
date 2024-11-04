1a729c6cd656291e208774b7cf2c945e03e7e741
Harugo Shintani <shin@shin-MBA.local>
2024-11-05 06:50:46 +0900

    add config.hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  
+  # バックエンドのホスト名を許可
+  config.hosts << "gpt-codecure-rails.onrender.com"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
